166d20db5bf97bbe3b8587c20816cc5ec62ffac6
adam <adam.jazwiec@gmail.com>
2019-09-10 20:34:09 +0200

    added tc1 in ts1

---
+++ Test Suites/TS1.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>ceb6dfb7-1984-4b91-8b76-d5c3da9e3754</testSuiteGuid>
+   <testCaseLink>
+      <guid>b2dd7f07-4b60-4614-8be8-829c67f9e3e2</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC1</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
